c698904fffcbd282aaa0dbe655a0e34f369c2488
Abolfazl Karimi <abolfazl.karimi@gmail.com>
2020-09-10 21:37:22 -0400

    Disparity working.

--- disparity_extender.py
+++ disparity_extender.py
@@ -10,22 +10,26 @@ from sklearn.cluster import KMeans
 
 actor_list = []
 waypoint = carla.Location()
-angle = 0
 target_cartesian = None
 # Order = vehicle, spectator camera, sensor1, sensor2
 
 
 def control_loop(world_snapshot, vehicle, controller, spectator, camera):
     global target_cartesian
-    # When all actors have been spawned
+
+    spectator.set_transform(camera.get_transform())
+
+    if (target_cartesian is None):
+        print("Target not calculated yet.")
+        return
+
+    # Calculate the control command based on target_cartesian
     speed = math.sqrt(vehicle.get_velocity().x **
                       2 + vehicle.get_velocity().y ** 2)
     throttle = controller.throttle_control(speed)
-    steer = 0
-    # steer = controller.cartesian_steering_control(target_cartesian)
+    steer = controller.cartesian_steering_control(target_cartesian)
     print("steer: " + str(steer))
     control = carla.VehicleControl(throttle, steer)
-    spectator.set_transform(camera.get_transform())
     vehicle.apply_control(control)
 
 
@@ -57,7 +61,7 @@ def main():
     # world = client.get_world()
     settings = world.get_settings()
     settings.synchronous_mode = True  # Enables synchronous mode
-    settings.fixed_delta_seconds = 0.005  # simulation time between two frames
+    settings.fixed_delta_seconds = 0.01  # simulation time between two frames
     world.apply_settings(settings)
 
     spectator = world.get_spectator()
@@ -67,8 +71,7 @@ def main():
     world.on_tick(lambda world_snapshot: control_loop(
         world_snapshot, vehicle, controller, spectator, camera))
 
-    # if (target_cartesian is not None):  # wait until first Lidar scan
-    tick_rate = 50.0  # number of ticks per second, assuming tick() runs in zero time
+    tick_rate = 100.0  # number of ticks per second, assuming tick() runs in zero time
     while True:
         time.sleep(1/tick_rate)
         world.tick()
@@ -81,12 +84,9 @@ def spawn_actor(world):
     waypoint = waypoints[500]
     waypoint = change_lane(waypoint, targetLane - waypoint.lane_id)
 
-    location = waypoint.transform.location + carla.Vector3D(0, 0, 1.5)
-    rotation = waypoint.transform.rotation
-
     # Set spawning location as initial waypoint
     blueprint = world.get_blueprint_library().filter('vehicle.*model3*')[0]
-    location = waypoint.transform.location + carla.Vector3D(0, 0, 1.5)
+    location = waypoint.transform.location + carla.Vector3D(0, 0, 0.5)
     rotation = waypoint.transform.rotation
     vehicle = world.spawn_actor(blueprint, carla.Transform(location, rotation))
     actor_list.append(vehicle)
@@ -103,11 +103,11 @@ def spawn_actor(world):
     # Add spectator camera
     camera_bp = world.get_blueprint_library().find('sensor.camera.rgb')
     camera_transform = carla.Transform(
-        carla.Location(x=-10, z=10), carla.Rotation(-30, 0, 0))
+        carla.Location(x=-10, z=10), carla.Rotation(-20, 0, 0))
     camera = world.spawn_actor(camera_bp, camera_transform, attach_to=vehicle)
     actor_list.append(camera)  # Add to actor_list at [1]
 
-    transform = carla.Transform(carla.Location(x=0.8, z=3))
+    transform = carla.Transform(carla.Location(x=0.8, z=2))
     attach_lidar(world, vehicle, transform)
     attach_collision_sensor(world, vehicle, transform)
 
@@ -134,7 +134,7 @@ def attach_lidar(world, vehicle, transform):
     actor_list.append(lidar_sensor)  # Add at actor_list[2]
 
     # Commented out to decrease processing
-    lidar_sensor.listen(lambda data: save_lidar_image(
+    lidar_sensor.listen(lambda data: process_lidar_data(
         data, world, vehicle))
 
     return lidar_sensor
@@ -166,109 +166,55 @@ def collision_event(data, world, vehicle):
     time.sleep(1)
 
 
-def save_lidar_image(image, world, vehicle):
+def process_lidar_data(image, world, vehicle):
     global target_cartesian
+
+    settings = world.get_settings()
+    t_step = settings.fixed_delta_seconds
 
     # Convert raw data to coordinates (x,y,z) in lidar's coords?
     points = np.frombuffer(image.raw_data, dtype=np.dtype('f4'))
     points = np.reshape(points, (int(points.shape[0] / 3), 3))
+
+    # To compensate for bug in carla 0.9.9.4:
+    # (x, y, z) to (-y, x, z) i.e. rotate 90 degrees counterclockwise
+    points = np.matmul(points, np.array([[0, 1, 0], [-1, 0, 0], [0, 0, 1]]))
+
+    # For easy enumeration and sorting.
     points = points.tolist()
+    # Sort the points by angle
+    points.sort(key=lambda point: np.arctan2(point[1], point[0]))
 
     lidar_transform = image.transform
-    lidar_loc = lidar_transform.location
-    forward = lidar_transform.get_forward_vector()
-    right = lidar_transform.get_right_vector()
-    up = lidar_transform.get_up_vector()
-    world.debug.draw_arrow(lidar_loc, lidar_loc + forward*10, life_time=0.02,
-                           color=carla.Color(255, 0, 0),
-                           thickness=0.4, arrow_size=0.4)
-    world.debug.draw_arrow(lidar_loc, lidar_loc + right*10, life_time=0.02,
-                           color=carla.Color(0, 255, 0),
-                           thickness=0.4, arrow_size=0.4)
-    world.debug.draw_arrow(lidar_loc, lidar_loc + up*10, life_time=0.02,
-                           color=carla.Color(0, 0, 255),
-                           thickness=0.4, arrow_size=0.4)
-
-    vehicle_transform = vehicle.get_transform()
-    vehicle_loc = vehicle_transform.location
-    forward = vehicle_transform.get_forward_vector()
-    right = vehicle_transform.get_right_vector()
-    up = vehicle_transform.get_up_vector()
-    world.debug.draw_arrow(vehicle_loc, vehicle_loc + forward*10, life_time=0.02,
-                           color=carla.Color(255, 0, 0),
-                           thickness=0.4, arrow_size=0.4)
-    world.debug.draw_arrow(vehicle_loc, vehicle_loc + right*10, life_time=0.02,
-                           color=carla.Color(0, 255, 0),
-                           thickness=0.4, arrow_size=0.4)
-    world.debug.draw_arrow(vehicle_loc, vehicle_loc + up*10, life_time=0.02,
-                           color=carla.Color(0, 0, 255),
-                           thickness=0.4, arrow_size=0.4)
-
-    for point in points:
-        loc = carla.Location(x=-point[1], y=point[0], z=point[2])
+    for index in range(len(points)-1):
+        point = points[index]
+        loc = carla.Location(x=point[0], y=point[1], z=point[2])
+        intensity = int(index/2)
         world.debug.draw_point(
-            lidar_transform.transform(loc), life_time=0.03, color=carla.Color(0, 255, 255))
+            lidar_transform.transform(loc), life_time=t_step, color=carla.Color(0, intensity, intensity))
 
-    # Sort the points by radius
-    points.sort(key=lambda point: (
-        np.arctan2(point[1], point[0]) * 180 / math.pi))
-    # for point in points:
-    # print((np.arctan2(point[1], point[0]) * 180  / math.pi))
-    for point in points:
-        point[0] = -point[0]
+    index1, index2, found = find_disparity(points)
+    if (not found):
+        print("No disparity found.")
+        return
 
-    # NOTE: some points have a negative angle, so sorting is seperated
-    # Might be worth fixing later
+    # visualize the disparity points
+    point1 = points[index1]
+    point2 = points[index2]
+    loc = carla.Location(x=point1[0], y=point1[1], z=point1[2])
+    world.debug.draw_point(
+        lidar_transform.transform(loc), size=0.2, life_time=t_step, color=carla.Color(255, 0, 0))
+    loc = carla.Location(x=point2[0], y=point2[1], z=point2[2])
+    world.debug.draw_point(
+        lidar_transform.transform(loc), size=0.2, life_time=t_step, color=carla.Color(255, 0, 0))
 
-    # Rotate into the car's frame
-    transform = vehicle.get_transform()
-    transform = [transform.location.x,
-                 transform.location.y, transform.location.z]
-    vehicle_rotation = vehicle.get_transform().rotation
-    roll = vehicle_rotation.roll
-    pitch = vehicle_rotation.pitch
-    yaw = vehicle_rotation.yaw + (np.pi / 2)
-    R = transforms3d.euler.euler2mat(roll, pitch, yaw).T
-
-    left_hand_points = [[p[0], -p[1], 0] for p in points]
-    world_points = [np.dot(R, point) for point in left_hand_points]
-    # Move location into car's frame
-    # world_points[:] = [np.add(transform, point) for point in world_points]
-
-    # index1, index2 = find_disparity(points)
-    # # Switched to points from world_points
-    # target_cartesian = points[index1]
-
-    # graph_cartesian_points(points, points[index1], points[index2])
-
-    # relative_loc = carla.Location(
-    #     x=world_points[index1][0], y=world_points[index1][1], z=0.0)
-    # # world.debug.draw_point(relative_loc, life_time=0.5)
-    # relative_loc = carla.Location(
-    #     x=world_points[index2][0], y=world_points[index2][1], z=0.0)
-    # world.debug.draw_point(relative_loc, life_time=0.5,
-    #                        color = carla.Color(0, 255, 255))
-
-    # print("TARGET: " + str(relative_loc))
-    # for point in world_points:
-    #     loc = carla.Location(x=point[0], y=point[1], z=0.0)
-    #     world.debug.draw_point(
-    #         loc, life_time=5, color=carla.Color(0, 255, 255))
-
-
-# def graph_cartesian_points(points, target_point, target_point_2):
-#     x = [point[0] for point in points]
-#     y = [point[1] for point in points]
-
-#     plt.scatter(x, y, marker='o', color='r')
-#     plt.plot(target_point[0], target_point[1], marker='o', color='g')
-#     plt.plot(target_point_2[0], target_point_2[1], marker='o', color='b')
-
-#     #plt.axis([-500, 1500, -800, 100])
-#     plt.style.use('seaborn-whitegrid')
-#     plt.savefig("cart_plot.png")
-#     plt.clf()
-#     #print("plot saved")
+    # TODO: change from lidar to vehicle's coordinates
+    target_cartesian = [(point1[0]+point2[0])*0.5,
+                        (point1[1]+point2[1])*0.5, (point1[2]+point2[2])*0.5]
+    loc = carla.Location(
+        x=target_cartesian[0], y=target_cartesian[1], z=target_cartesian[2])
+    world.debug.draw_point(
+        lidar_transform.transform(loc), size=0.2, life_time=t_step, color=carla.Color(0, 255, 0))
 
 
 class Pure_Pursuit_Controller():
@@ -282,10 +228,11 @@ class Pure_Pursuit_Controller():
         self.max_steer = max_steer
 
     def throttle_control(self, speed):
-        if speed >= 30:
-            return 0.7
-        else:
-            return 1.0
+        return 3.0
+        # if speed >= 30:
+        #     return 0.7
+        # else:
+        #     return 1.0
 
     def steering_control(self, vehicle_tr, waypoint_tr):
         wp_loc_rel = relative_location(
@@ -302,46 +249,45 @@ class Pure_Pursuit_Controller():
 
         return steer / self.max_steer
 
+    # The point coordinates are in vehicle's frame.
     def cartesian_steering_control(self, cartesian_point):
         x = cartesian_point[0]
-        if x < 0:
-            x = x + 3
-        if x > 0:
-            x = x - 3
-        y = cartesian_point[1] * 2.5  # Added a scale/extension of 300%
-        steer_rad = math.atan(x / y)  # Changed from tan to atan
+        y = cartesian_point[1]
+        steer_rad = math.atan(y / x)
         steer_deg = np.degrees(steer_rad)
-        print(steer_deg)
         steer = np.clip(steer_deg, -self.max_steer, self.max_steer)
-        if abs(steer_deg) < 50:
-            return steer_deg / (2 * self.max_steer)
-        if abs(steer_deg) < 5:
-            return 0
 
         return steer / self.max_steer
 
 
-def find_disparity(cartesian_coordinates):
-    # cartesian coordinates are in the car's reference frame
-    max_distance = 0
-    max_disparity_pair = [[], []]
-    # The disparities are in the form of polar coordinates, so
-    # the pair is in the form [[radius1, degrees1, height], [radius2, degrees2, height]
-    # max_disparity_pair[0] is to the left of max_disparity_pair[1]
+"""Find the biggest disparity
 
-    for i in range(len(cartesian_coordinates) - 1):
-        if cartesian_coordinates[i][1] > 0:
-            x1 = cartesian_coordinates[i][0]
-            y1 = cartesian_coordinates[i][1]
-            x2 = cartesian_coordinates[i+1][0]
-            y2 = cartesian_coordinates[i+1][1]
-            distance = math.sqrt((x1-x2)**2 + (y1-y2)**2)
-            if distance > max_distance and distance > 5:
-                max_distance = distance
+The given points are sorted by their angle around the down (up) vector in the lidar's frame.
+"""
+
+
+def find_disparity(points):
+    # Default value is in the middle of the array which should be in front of the car.
+    index1 = 0
+    index2 = 0
+    found = False
+
+    min_d2 = 25  # A disparity must be at least 5 meters
+    max_d2 = 0
+    for i in range(len(points) - 1):
+        if points[i][0] > 0:  # Ignore points behind lidar
+            x1 = points[i][0]
+            y1 = points[i][1]
+            x2 = points[i+1][0]
+            y2 = points[i+1][1]
+            d2 = (x1-x2)**2 + (y1-y2)**2
+            if d2 > max_d2 and d2 > min_d2:  # Found a better disparity
+                found = True
+                max_d2 = d2
                 index1 = i
                 index2 = i + 1
 
-    return index1, index2
+    return index1, index2, found
 
 
 """Change coordinates
